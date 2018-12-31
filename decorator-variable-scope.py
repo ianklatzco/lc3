fdcccf561d491d2e44f0e887b003156b4deef2ce
Ian Klatzco <iklatzco@gmail.com>
2018-12-31 13:22:25 -0900

    haha! it works! no giving up

--- decorator-variable-scope.py
+++ decorator-variable-scope.py
@@ -10,16 +10,14 @@ def extract_opcode(func):
     @functools.wraps(func) # preserve docstring, name of orig function
     def wrapper(*args, **kwargs):
         opcode = args[0]
-        # print(hex(opcode))
         func.opcode = opcode
+        func.__kwdefaults__ = {'self': func}
         return func(*args, **kwargs)
-    wrapper.__kwdefaults__ = {'self': func}
     return wrapper
 
 @extract_opcode
 def ins(instr, *, self = 3): # asterisk: everything after this is kw only
-    # print(hex(self.opcode))
-    print(self)
+    print(hex(self.opcode))
     pass
 
 # ins.__kwdefaults__ = {'self': ins}
