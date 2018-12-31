0661c26f4cf617b4d726184a0028d7dc577e98c8
Ian Klatzco <iklatzco@gmail.com>
2018-12-31 10:36:32 -0900

    rearrange code layout

--- lc3.py
+++ lc3.py
@@ -2,6 +2,7 @@ from ctypes import c_uint16, c_int16
 from enum import IntEnum
 from struct import unpack
 from sys import exit, stdin, stdout
+from signal import signal, SIGINT
 
 # https://justinmeiners.github.io/lc3-vm/
 
@@ -21,9 +22,63 @@ from sys import exit, stdin, stdout
 DEBUG = False
 
 # https://stackoverflow.com/a/32031543/1234621
+# you're modeling sign-extend behavior in python, since python has infinite
+# bit width.
 def sext(value, bits):
     sign_bit = 1 << (bits - 1)
     return (value & (sign_bit - 1)) - (value & sign_bit)
+
+'''
+iirc the arch is 16bit little endian.
+options: ctypes or just emulate it in pure python.
+chose: ctypes
+'''
+class memory():
+    def __init__(self):
+        # ctypes has an array type. this is one way to create instances of it.
+        self.memory = (c_uint16 * 65536)()
+
+    def __getitem__(self, arg):
+        if (arg > 65535) or (arg < 0):
+            raise MemoryError("Accessed out valid memory range.")
+
+        return self.memory[arg]
+
+    def __setitem__(self, location, thing_to_write):
+        if (location > 65536) or (location < 0):
+            raise MemoryError("Accessed out valid memory range.")
+
+        self.memory[int(location)] = thing_to_write
+
+class registers():
+    def __init__(self):
+        self.gprs = (c_int16 * 8)()
+        self.pc = (c_uint16)()
+        self.cond = (c_uint16)()
+
+# not actually a class but an enum.
+class opcodes(IntEnum):
+    op_br = 0
+    op_add = 1
+    op_ld = 2
+    op_st = 3
+    op_jsr = 4
+    op_and = 5
+    op_ldr = 6
+    op_str = 7
+    op_rti = 8
+    op_not = 9
+    op_ldi = 10
+    op_sti = 11
+    op_jmp = 12
+    op_res = 13
+    op_lea = 14
+    op_trap = 15
+
+class condition_flags(IntEnum):
+    p = 0
+    z = 1
+    n = 2
 
 class lc3():
     def __init__(self, filename):
@@ -258,59 +313,7 @@ class lc3():
             else:
                 raise Error("invalid opcode")
 
-
-'''
-iirc the arch is 16bit little endian.
-options: ctypes or just emulate it in pure python.
-chose: ctypes
-'''
-class memory():
-    def __init__(self):
-        # ctypes has an array type. this is one way to create instances of it.
-        self.memory = (c_uint16 * 65536)()
-
-    def __getitem__(self, arg):
-        if (arg > 65535) or (arg < 0):
-            raise MemoryError("Accessed out valid memory range.")
-
-        return self.memory[arg]
-
-    def __setitem__(self, location, thing_to_write):
-        if (location > 65536) or (location < 0):
-            raise MemoryError("Accessed out valid memory range.")
-
-        self.memory[int(location)] = thing_to_write
-
-class registers():
-    def __init__(self):
-        self.gprs = (c_int16 * 8)()
-        self.pc = (c_uint16)()
-        self.cond = (c_uint16)()
-
-# not actually a class but an enum.
-class opcodes(IntEnum):
-    op_br = 0
-    op_add = 1
-    op_ld = 2
-    op_st = 3
-    op_jsr = 4
-    op_and = 5
-    op_ldr = 6
-    op_str = 7
-    op_rti = 8
-    op_not = 9
-    op_ldi = 10
-    op_sti = 11
-    op_jmp = 12
-    op_res = 13
-    op_lea = 14
-    op_trap = 15
-
-class condition_flags(IntEnum):
-    p = 0
-    z = 1
-    n = 2
-
+##############################################################################
 
 l = lc3("second.obj")
 l.start()
