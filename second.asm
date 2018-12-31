0b7180fd1d61855966020965b6545ab3dd4b261d
Ian Klatzco <iklatzco@gmail.com>
2018-12-30 18:51:20 -0900

    update test program

--- second.asm
+++ second.asm
@@ -1,13 +1,9 @@
 ; with thanks to https://stackoverflow.com/a/33401821/1234621
 ; and the patt/patel textbook
 .ORIG x3000
-  LEA R0, NUM       ; pointer [mem]NUM
+  LEA R0, STRING       ; pointer [mem]NUM
   PUTs              ; print our string starting from [mem]address in R0
   HALT              ; Trap x25
 
-NUM   .fill  x31    ; Our Number to print
-      .fill  x32     
-      .fill  x33
-      .fill  x34
-      .fill  x0a
+STRING  .stringz  "1234\n"
 .END
