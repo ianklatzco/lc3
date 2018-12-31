1b3ea7a64fe91a50b532322ac2a59e029408c4f0
Ian Klatzco <iklatzco@gmail.com>
2018-12-30 20:50:06 -0900

    add is done

--- lc3.py
+++ lc3.py
@@ -2,6 +2,7 @@ from ctypes import c_uint16
 from enum import IntEnum
 from binascii import *
 from struct import unpack
+from sys import exit
 
 # https://justinmeiners.github.io/lc3-vm/
 
@@ -25,79 +26,96 @@ class lc3():
         for count in range(0,len(c), 2):
             self.memory[0x3000+count/2] = unpack( '>H', c[count:count+2] )[0]
 
-    def op_add_impl(self):
+    def op_add_impl(self, instruction):
+        sr1 = (instruction >> 6) & 0b111
+        dr  = (instruction >> 9) & 0b111
+        if ((instruction >> 5) & 0b1) == 0: # reg-reg
+            sr2 = instruction & 0b111
+            self.registers.gprs[dr] = self.registers.gprs[sr1] + self.registers.gprs[sr2]
+        else: # immediate
+            imm5 = instruction & 0b11111 
+            self.registers.gprs[dr] = self.registers.gprs[sr1] + imm5
+    def op_and_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_and_impl(self):
+    def op_not_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_not_impl(self):
+    def op_br_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_br_impl(self):
+    def op_jmp_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_jmp_impl(self):
+    def op_jsr_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_jsr_impl(self):
+    def op_ld_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_ld_impl(self):
+    def op_ldr_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_ldr_impl(self):
+    def op_lea_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_lea_impl(self):
+    def op_st_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_st_impl(self):
+    def op_sti_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_sti_impl(self):
+    def op_str_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_str_impl(self):
+    def op_trap_impl(self, instruction):
+        # todo: implement more than just halt
+        print("r0: {:05} ".format(self.registers.gprs[0]), end='')
+        print("r1: {:05} ".format(self.registers.gprs[1]), end='')
+        print("r2: {:05} ".format(self.registers.gprs[2]), end='')
+        print("r3: {:05} ".format(self.registers.gprs[3]), end='')
+        print("r4: {:05} ".format(self.registers.gprs[4]), end='')
+        print("r5: {:05} ".format(self.registers.gprs[5]), end='')
+        print("r6: {:05} ".format(self.registers.gprs[6]), end='')
+        print("r7: {:05}".format(self.registers.gprs[7]))
+        exit()
+    def op_res_impl(self, instruction):
         raise Error("unimplemented opcode")
-    def op_trap_impl(self):
-        raise Error("unimplemented opcode")
-    def op_res_impl(self):
-        raise Error("unimplemented opcode")
-    def op_rti_impl(self):
+    def op_rti_impl(self, instruction):
         raise Error("unimplemented opcode")
 
     def start(self):
         running = 1
         while running < 10:
             # fetch instruction
-            fetched_instruction = self.memory[self.registers.pc.value]
+            instruction = self.memory[self.registers.pc.value]
 
             # update PC
             self.registers.pc.value = self.registers.pc.value + 1
 
             # decode opcode
-            opcode = fetched_instruction >> 12
+            opcode = instruction >> 12
 
+            # decoding of the instruction should happen here
             if opcode == opcodes.op_add:
-                self.op_add_impl()
+                self.op_add_impl(instruction)
             elif opcode == opcodes.op_and:
-                self.op_and_impl()
+                self.op_and_impl(instruction)
             elif opcode == opcodes.op_not:
-                self.op_not_impl()
+                self.op_not_impl(instruction)
             elif opcode == opcodes.op_br:
-                self.op_br_impl()
+                self.op_br_impl(instruction)
             elif opcode == opcodes.op_jmp:
-                self.op_jmp_impl()
+                self.op_jmp_impl(instruction)
             elif opcode == opcodes.op_jsr:
-                self.op_jsr_impl()
+                self.op_jsr_impl(instruction)
             elif opcode == opcodes.op_ld:
-                self.op_ld_impl()
+                self.op_ld_impl(instruction)
             elif opcode == opcodes.op_ldr:
-                self.op_ldr_impl()
+                self.op_ldr_impl(instruction)
             elif opcode == opcodes.op_lea:
-                self.op_lea_impl()
+                self.op_lea_impl(instruction)
             elif opcode == opcodes.op_st:
-                self.op_st_impl()
+                self.op_st_impl(instruction)
             elif opcode == opcodes.op_sti:
-                self.op_sti_impl()
+                self.op_sti_impl(instruction)
             elif opcode == opcodes.op_str:
-                self.op_str_impl()
+                self.op_str_impl(instruction)
             elif opcode == opcodes.op_trap:
-                self.op_trap_impl()
+                self.op_trap_impl(instruction)
             elif opcode == opcodes.op_res:
-                self.op_res_impl()
+                self.op_res_impl(instruction)
             elif opcode == opcodes.op_rti:
-                self.op_rti_impl()
+                self.op_rti_impl(instruction)
             else:
                 raise Error("invalid opcode")
 
@@ -124,20 +142,14 @@ class memory():
         if (location > 65536) or (location < 0):
             raise MemoryError("Accessed out valid memory range.")
 
-        self.memory[location] = thing_to_write
+        self.memory[int(location)] = thing_to_write
 
 class registers():
     def __init__(self):
-        self.r0 = (c_uint16)()
-        self.r1 = (c_uint16)()
-        self.r2 = (c_uint16)()
-        self.r3 = (c_uint16)()
-        self.r4 = (c_uint16)()
-        self.r5 = (c_uint16)()
-        self.r6 = (c_uint16)()
-        self.r7 = (c_uint16)()
+        self.gprs = (c_uint16 * 8)()
         self.pc = (c_uint16)()
         self.cond = (c_uint16)()
+
 
 # not actually a class but an enum.
 class opcodes(IntEnum):
--- second.asm
+++ second.asm
@@ -1,9 +1,11 @@
 ; with thanks to https://stackoverflow.com/a/33401821/1234621
 ; and the patt/patel textbook
 .ORIG x3000
-  ADD R0, R0, #0
-  LEA R0, STRING       ; pointer [mem]NUM
-  PUTs              ; print our string starting from [mem]address in R0
+  ADD R0, R0, #3
+  ADD R2, R2, #2
+  ADD R1, R2, R0
+  ; LEA R0, STRING       ; pointer [mem]NUM
+  ; PUTs              ; print our string starting from [mem]address in R0
   HALT              ; Trap x25
 
 STRING  .stringz  "1234\n"
